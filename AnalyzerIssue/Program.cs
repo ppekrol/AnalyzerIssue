42ac97eb58ff3d8de61ac8e70bbbaad797cc4547
Pawel Pekrol <ppekrol@ravendb.net>
2017-06-13 15:23:27 +0200

    initial commit

---
+++ AnalyzerIssue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AnalyzerIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private async Task Sample()
+        {
+            await Task.Delay(100).ConfigureAwait(false);
+        }
+    }
+}
